9cf3225397ffa16cc9f30dd505bef85da08b28ee
Dario Iacampo <dario@jntstudio.net>
2018-07-17 15:40:21 +0200

    Bump version: 0.1.4 → 0.1.5

--- pull_webhook/__init__.py
+++ pull_webhook/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Dario Iacampo"""
 __email__ = 'dario@jntstudio.net'
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 
 from .pull_webhook import Puller
 from .pull_webhook import main
